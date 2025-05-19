7427a4f0dbc0e5ae42d7c7b9ae9bd36821687826
baiyuxiang12 <byx100116@163.com>
2025-05-20 06:44:28 +0800

    Update BlockBreakMixin.java

--- src/main/java/sakuyaayane/nekoantixray/mixin/BlockBreakMixin.java
+++ src/main/java/sakuyaayane/nekoantixray/mixin/BlockBreakMixin.java
@@ -1,34 +1,41 @@
 package sakuyaayane.nekoantixray.mixin;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import sakuyaayane.nekoantixray.NekoAntiXray;
 
-@Mixin(PlayerEntity.class)
+/**
+ * 方块破坏Mixin - 监听玩家挖掘方块事件
+ */
+@Mixin(ServerPlayerInteractionManager.class)
 public class BlockBreakMixin {
-
-    @Inject(method = "breakBlock", at = @At("HEAD"))
-    private void onBlockBreak(BlockPos pos, CallbackInfo ci) {
-        // Cast this to PlayerEntity since we're in a mixin
-        PlayerEntity player = (PlayerEntity)(Object)this;
-        
-        // Only process on server side and for server players
-        if (player.getWorld().isClient || !(player instanceof ServerPlayerEntity)) {
-            return;
+    
+    @Shadow
+    public ServerPlayerEntity player;
+    
+    /**
+     * 注入到tryBreakBlock方法，监听玩家挖掘方块
+     */
+    @Inject(method = "tryBreakBlock", at = @At("HEAD"))
+    private void onBlockBreak(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if (player != null && player.getWorld() != null) {
+            World world = player.getWorld();
+            
+            // 检查是否为假矿石
+            if (NekoAntiXray.getInstance().getFakeOreManager().isFakeOre(pos)) {
+                // 处理玩家挖掘假矿石
+                NekoAntiXray.getInstance().getDetectionManager().handleFakeOreMined(player, pos);
+                // 移除假矿石记录
+                NekoAntiXray.getInstance().getFakeOreManager().removeFakeOre(pos);
+            }
         }
-        
-        ServerPlayerEntity serverPlayer = (ServerPlayerEntity)player;
-        ServerWorld world = (ServerWorld)player.getWorld();
-        BlockState state = world.getBlockState(pos);
-        
-        // Notify the detection manager about the block break
-        NekoAntiXray.getInstance().getDetectionManager().onBlockBreak(serverPlayer, world, pos, state);
     }
 }
